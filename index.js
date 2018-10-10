bd0862a7bc919b7d0a5eede50ba0c885111a056a
Глебка Фокин <g.fokin@n1.ru>
2018-10-10 23:26:32 +0700

    Handle slow change in tickets amount

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const _ = require("lodash");
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
@@ -8,6 +9,14 @@ const chatId = process.env.CHAT_ID;
 const interval = process.env.INTERVAL || 10000;
 const term = process.env.SEARCH_TERM || "ФАН";
 const bot = new TelegramBot(token, { polling: true });
+
+if (!token) {
+  throw new Error("Не указан API токен бота");
+}
+
+if (!chatId) {
+  throw new Error("Не указан chatId телеграм канала");
+}
 
 const parseSector = $ => el => {
   const name = $(el)
@@ -43,7 +52,8 @@ const sendMessageToTelegram = async message => {
   }
 };
 
-let prevAmount = 99999;
+let prevAmount = 0;
+let fullSectorName = term;
 
 const checkTickets = async () => {
   try {
@@ -51,17 +61,28 @@ const checkTickets = async () => {
     const amount = _.get(sector, "amount", 0);
     const name = _.get(sector, "name", term);
 
-    if (amount !== prevAmount) {
-      const message = `Обновились билеты в ${name}, осталось билетов: ${amount}.\nКупить: https://metallica.kassir.ru/koncert/metallica`;
+    const isNewBatch = prevAmount === 0 && amount !== prevAmount;
+    const isRunOut = amount === 0 && amount !== prevAmount;
+    const isHalfOfBatch = prevAmount / amount > 2;
+
+    if (isNewBatch || isHalfOfBatch || isRunOut) {
+      // Сохраним имя сектора целиком, чтобы красиво его показывать потом
+      fullSectorName = name;
+      prevAmount = amount;
+
+      const message = amount
+        ? `В наличии билеты в ${name}, осталось билетов: ${amount}.\nКупить: https://metallica.kassir.ru/koncert/metallica`
+        : `Билеты в ${name} сейчас нет! :(`;
       await sendMessageToTelegram(message);
-      console.log(`Обновилось количество в ${name}, осталось билетов: ${amount}`);
+
+      console.log(
+        `Обновилось количество в ${name}, осталось билетов: ${amount}`
+      );
     } else {
       console.log("Количество билетов не обновилось.");
     }
-
-    prevAmount = amount;
   } catch (e) {
-    console.log('Ошибка: ', e.message);
+    console.log("Ошибка: ", e.message);
   }
 };
 
